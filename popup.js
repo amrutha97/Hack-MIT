17c7897b63a6bf6b52fe6adaa25e158e3d2e144a
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2015-09-20 01:06:03 -0400

    Integrate Sushi code and Amrutha code

--- popup.js
+++ popup.js
@@ -1,24 +1,24 @@
 //Creates JSON lcoation object
 
-chrome.tabs.query(function(tabs) {    
-    var tab = tabs[0]; 
+chrome.tabs.query(function(tabs) {
+    var tab = tabs[0];
     var url = tab.url;
     console.assert(typeof url == 'string', 'tab.url should be a string');
-      
+
       //IF IT BREAKS- check if url pathname is correct
-      var locationArray = url.pathname.substring((window.location.pathname.indexOf('@')+1)).split(',');
+      var locationArray = url.pathname.substring((url.location.pathname.indexOf('@')+1)).split(',');
       var locationLatitude = locationArray[0];
       var locationLongitude = locationArray[1];
-          
+
       var location = {
         latitude: locationLatitude,
         longitude: locationLongitude
-      }   
+      }
       return location;
 });
 
 
-//get current location 
+//get current location
 
 function getCurrentLocation() {
 }
@@ -26,6 +26,50 @@ function getCurrentLocation() {
 
 function computePrice(location) {
     //Sushi's code that accesses the Uber API
+    var token = "kTKiSbrlx7zJohVcTBYYedeYliZAdwMlo6d4kV9r";
+
+function getEstimate(start_latitude, start_longitude, end_latitude, end_longitude, renderResult) {
+$.ajax ({
+   url: "https://api.uber.com/v1/estimates/price",
+    method: "GET",
+    data: {
+        start_latitude: start_latitude,
+        start_longitude: start_longitude,
+        end_latitude: end_latitude,
+        end_longitude: end_longitude,
+
+    },
+    headers: {
+        Authorization: "Token " + token
+    },
+    success: function(data) {
+        var result = getResult(data);
+        renderResult(result);
+    }
+})
+}
+
+
+
+
+function getResult(data) {
+        var price = data["prices"]["estimate"];
+        return price;
+    }
+
+
+//var token = "kTKiSbrlx7zJohVcTBYYedeYliZAdwMlo6d4kV9r";
+//var xhr = new XMLHttpRequest();
+//var response = xhr.open('GET', 'https://api.uber.com/v1/products?latitude=37.7759792&longitude=-122.41823');
+//xhr.setRequestHeader("Authorization", "Token "+token);
+//xhr.setRequestHeader('Content-Type', 'application/json');
+//response = xhr.send();
+//debugger;
+//response;
+//if(typeof(response) !== 'undefined') {
+//    alert(response);
+//}
+
 }
 
 function renderStatus(statusText) {
